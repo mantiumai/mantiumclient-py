dfb85b6024b4b12e4c31c5b0178b6d9ed1fde736
j0monty <jason@mantiumai.com>
2021-07-19 15:02:31 -0400

    Adjusted license header
    Tweaked license banner on each file.

--- mantiumapi/intelet.py
+++ mantiumapi/intelet.py
@@ -12,7 +12,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
+#
 # Please refer to our terms for more information:
 #     https://mantiumai.com/terms-of-use/
 #
