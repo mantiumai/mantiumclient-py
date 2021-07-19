419162e99e33f78b93fb8254a1f2a96895e6520a
j0monty <jason@mantiumai.com>
2021-07-19 14:56:31 -0400

    Added licensing info to all files.
    Add license and ToS Banner to all files.

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2021 Mantium, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# Please refer to our terms for more information:
+#     https://mantiumai.com/terms-of-use/
+#
 import os
 import setuptools
 
@@ -8,7 +26,7 @@ with open('README.md', 'r') as fh:
 build_number = os.getenv('TRAVIS_BUILD_NUMBER', '1')
 
 setuptools.setup(name='mantiumapi',
-    version='0.0.' + build_number,
+    version='0.1.' + build_number,
     description='Python Client for the Mantium API',
     long_description='This software is provided as a way to include the Mantium API functionality in your own Python '
                      'software. You can read about the Mantium API at https://developer.mantiumai.com/',
@@ -20,7 +38,7 @@ setuptools.setup(name='mantiumapi',
     install_requires=DEPENDENCIES,
     keywords=['security', 'ai', 'nlp'],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
@@ -30,9 +48,11 @@ setuptools.setup(name='mantiumapi',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
         'Topic :: Scientific/Engineering',
         'Operating System :: OS Independent',
     ],
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
+    python_requires=">=3.6",
 )
