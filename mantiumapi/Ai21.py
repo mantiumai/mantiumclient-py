a3e16623be8b7a6c9b044d26132015174fb8b2fa
Svitlana Glibova <svitlana.glibova@Svitlanas-MacBook-Pro.local>
2021-11-08 16:49:49 -0800

    added utils file for helper functions

--- mantiumapi/Ai21.py
+++ mantiumapi/Ai21.py
@@ -6,15 +6,22 @@ from .engine_id_values import default_ai_engines
 from enum import Enum
 
 
+def get_engine_id(engine_name):
+    """Get Engine ID value from engine_id_values."""
+
+    engine_id = next(engine for engine in default_ai_engines if engine['name'] == engine_name)
+    return engine_id if engine_id else None
+
+
 class DefaultEngine(str, Enum):
-    """Must have a default engine"""
+    """Must have a default engine, constrained to engines available to AI21."""
 
     j1_jumbo = 'j1_jumbo'
     j1_large = 'j1_large'
 
 
 class AiMethod(Enum):
-    """Must have an endpoint (ai_method)"""
+    """Must have an endpoint (ai_method), constrained to endpoints available to AI21."""
 
     complete = 'complete'
 
@@ -22,13 +29,20 @@ class AiMethod(Enum):
 class Ai21(Prompt):
 
     """
-    default_engine: j1_jumbo, j1_large
-    ai_method: complete
+    AI21 Prompt class, inherits from Prompt.
+    Required parameters: default_engine, ai_method
+
+    default_engine options: j1_jumbo, j1_large
+    ai_method options: complete
 
     ***Settings***
     maxTokens: Max 2048 (incl. input)
     temperature: [0,1] inclusive
-    topP: [0,1] inclusive"""
+    topP: [0,1] inclusive
+
+    ***Example Prompt Instance***
+    ai_21_prompt = Ai21(default_engine=DefaultEngine.j1_jumbo, ai_method=AiMethod.complete)
+    """
 
     ai_provider = 'ai21'
 
@@ -38,6 +52,7 @@ class Ai21(Prompt):
             raise Exception('default_engine must be one of: j1_jumbo|j1_large')
 
         self.default_engine = default_engine.value
+        self.ai_engine_id = get_engine_id(self.default_engine)
 
         if not isinstance(ai_method, AiMethod):
             raise Exception('ai_method must be: complete')
--- mantiumapi/engine_id_values.py
+++ mantiumapi/engine_id_values.py
@@ -1,25 +1,69 @@
 default_ai_engines = [
-    {'ai_engine_id': 'b2ffecf7-4fee-42e7-b85d-a5e28d939396', 'name': 'davinci', 'ai_provider': 'OpenAI'},
-    {'ai_engine_id': 'b26dd8d0-efc4-45ef-899a-fad0ff93e258', 'name': 'curie', 'ai_provider': 'OpenAI'},
-    {'ai_engine_id': 'aa88258f-adbe-4259-9992-232bb5ff75b4', 'name': 'babbage', 'ai_provider': 'OpenAI'},
+    {
+        'ai_engine_id': 'b2ffecf7-4fee-42e7-b85d-a5e28d939396',
+        'name': 'davinci',
+        'ai_provider': 'OpenAI',
+    },
+    {
+        'ai_engine_id': 'b26dd8d0-efc4-45ef-899a-fad0ff93e258',
+        'name': 'curie',
+        'ai_provider': 'OpenAI',
+    },
+    {
+        'ai_engine_id': 'aa88258f-adbe-4259-9992-232bb5ff75b4',
+        'name': 'babbage',
+        'ai_provider': 'OpenAI',
+    },
     {
         'ai_engine_id': 'ce6850ef-bc78-4f7a-af5f-81fb9d9fb872',
         'name': 'ada',
         'ai_provider': 'OpenAI',
     },
-    {'ai_engine_id': 'a5b48665-a180-4c3c-9e02-a5506317c13a', 'name': 'davinci-codex', 'ai_provider': 'OpenAI'},
-    {'ai_engine_id': 'b18395a0-191c-4b85-a9a3-159facd83c28', 'name': 'cushman-codex', 'ai_provider': 'OpenAI'},
+    {
+        'ai_engine_id': 'a5b48665-a180-4c3c-9e02-a5506317c13a',
+        'name': 'davinci-codex',
+        'ai_provider': 'OpenAI',
+    },
+    {
+        'ai_engine_id': 'b18395a0-191c-4b85-a9a3-159facd83c28',
+        'name': 'cushman-codex',
+        'ai_provider': 'OpenAI',
+    },
     {
         'ai_engine_id': '34dd5e1f-2557-4f7f-b5b2-d3eccfb00ad7',
         'name': 'content-filter-alpha-c4',
         'ai_provider': 'OpenAI',
     },
-    {'ai_engine_id': '2d061d32-ac88-4934-8e30-88f7c1710bbb', 'name': 'content-filter-dev', 'ai_provider': 'OpenAI'},
-    {'ai_engine_id': '30cc43da-1724-4988-b6dd-72a29328a578', 'name': 'curie-instruct-beta', 'ai_provider': 'OpenAI'},
-    {'ai_engine_id': '78399e12-fa98-4dd5-8800-2fc41b28033f', 'name': 'cursing-filter-v6', 'ai_provider': 'OpenAI'},
-    {'ai_engine_id': '3f7e30f4-46b6-4338-b825-ffb4e87f69c8', 'name': 'davinci-instruct-beta', 'ai_provider': 'OpenAI'},
-    {'ai_engine_id': 'a7beb6e1-a107-41ad-b7a1-f1ba97d064a3', 'name': 'baseline-shrimp', 'ai_provider': 'Cohere'},
-    {'ai_engine_id': 'd211007a-ba0f-4f08-a528-11ec74cd2d71', 'name': 'baseline-otter', 'ai_provider': 'Cohere'},
+    {
+        'ai_engine_id': '2d061d32-ac88-4934-8e30-88f7c1710bbb',
+        'name': 'content-filter-dev',
+        'ai_provider': 'OpenAI',
+    },
+    {
+        'ai_engine_id': '30cc43da-1724-4988-b6dd-72a29328a578',
+        'name': 'curie-instruct-beta',
+        'ai_provider': 'OpenAI',
+    },
+    {
+        'ai_engine_id': '78399e12-fa98-4dd5-8800-2fc41b28033f',
+        'name': 'cursing-filter-v6',
+        'ai_provider': 'OpenAI',
+    },
+    {
+        'ai_engine_id': '3f7e30f4-46b6-4338-b825-ffb4e87f69c8',
+        'name': 'davinci-instruct-beta',
+        'ai_provider': 'OpenAI',
+    },
+    {
+        'ai_engine_id': 'a7beb6e1-a107-41ad-b7a1-f1ba97d064a3',
+        'name': 'baseline-shrimp',
+        'ai_provider': 'Cohere',
+    },
+    {
+        'ai_engine_id': 'd211007a-ba0f-4f08-a528-11ec74cd2d71',
+        'name': 'baseline-otter',
+        'ai_provider': 'Cohere',
+    },
     {
         'ai_engine_id': '4718cd5f-8c41-489a-8939-7a3e86a15036',
         'name': 'baseline-seal',
@@ -35,7 +79,19 @@ default_ai_engines = [
         'name': 'baseline-orca',
         'ai_provider': 'Cohere',
     },
-    {'ai_engine_id': '8745e877-aa89-499a-a364-072473b9cf07', 'name': 'iron', 'ai_provider': 'Mantium'},
-    {'ai_engine_id': '2d11947c-8407-4438-ae2b-63021a726bb9', 'name': 'j1-large', 'ai_provider': 'Ai21'},
-    {'ai_engine_id': '5dbbc52a-147f-4920-b8b0-ef198c9888a3', 'name': 'j1-jumbo', 'ai_provider': 'Ai21'},
+    {
+        'ai_engine_id': '8745e877-aa89-499a-a364-072473b9cf07',
+        'name': 'iron',
+        'ai_provider': 'Mantium',
+    },
+    {
+        'ai_engine_id': '2d11947c-8407-4438-ae2b-63021a726bb9',
+        'name': 'j1-large',
+        'ai_provider': 'Ai21',
+    },
+    {
+        'ai_engine_id': '5dbbc52a-147f-4920-b8b0-ef198c9888a3',
+        'name': 'j1-jumbo',
+        'ai_provider': 'Ai21',
+    },
 ]
